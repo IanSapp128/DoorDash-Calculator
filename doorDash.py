29afb460b47d151734be377b1be5f2f4088903e6
Ian <IanSapp128@gmail.com>
2024-01-05 13:17:35 -0600

    Updated to calculate num of people in each party

--- doorDash.py
+++ doorDash.py
@@ -3,16 +3,21 @@ import locale
 
 def main() -> None:
     locale.setlocale(locale.LC_ALL, "")
-    recipients = int(input("Number of recipiants: "))
+    recipients = int(input("Number of parties: "))
+    individuals = 0
     theDict = {}
+    theDict2 = {}
 
     # Loop through the number of people and get their names
     # Empty input will skip that person (For myself usually. I don't need to know my own total)
     for i in range(recipients):
-        name = input(f"Name for person {i + 1}: ")
+        name = input(f"Name for party {i + 1}: ")
         if name == "":
             pass
         else:
+            people = input(f"How many people in this party?")
+            theDict2[name] = int(people)
+            individuals += int(people)
             stopLoop = False
             items = 0
             while not stopLoop:
@@ -26,18 +31,22 @@ def main() -> None:
                     item = float(item)
                     items += item
 
-    fees = float(input("Doordash fees: "))
+    regFees = float(input("Regulatory fee: "))
+    deliveryFee = float(input("Delivery fee: "))
+    serviceFee = float(input("Service fee: "))
     tax = float(input("Taxes: "))
     tip = float(input("Tip: "))
 
     # Add fees to each person's total and show the output
-    print("===========================")
+    print("=========Totals=========")
     for key in theDict:
-        theDict[key] += float(fees / recipients)
-        theDict[key] += float(tax / recipients)
-        theDict[key] += float(tip / recipients)
+        theDict[key] += float((regFees / individuals) * theDict2[key])
+        theDict[key] += float((deliveryFee / individuals) * theDict2[key])
+        theDict[key] += float((serviceFee / individuals) * theDict2[key])
+        theDict[key] += float((tax / individuals) * theDict2[key])
+        theDict[key] += float((tip / individuals) * theDict2[key])
         print(f"{key}: {locale.currency(theDict[key], grouping=True)}")
-    print("===========================")
+    print("========================")
 
 
 if __name__ == "__main__":
